84473428ef52f9cd0e6e152a3a3f94769ad90dd9
Himanshu Borah <borahhimanshu3@gmail.com>
2022-07-30 12:14:46 +0530

    --- UPDATE ---

--- admin/admin.php
+++ admin/admin.php
@@ -3,7 +3,7 @@
 include BORAHH_ADL_TOURNAMENTS_DIR_ADMIN . 'menu/menu.php';    
 include BORAHH_ADL_TOURNAMENTS_DIR_ADMIN . 'types/tournaments.php';    
 include BORAHH_ADL_TOURNAMENTS_DIR_ADMIN . 'types/entry-pages.php';    
-include BORAHH_ADL_TOURNAMENTS_DIR_ADMIN . 'types/woo.php';    
+// include BORAHH_ADL_TOURNAMENTS_DIR_ADMIN . 'types/woo.php';    
 // include BORAHH_ADL_TOURNAMENTS_DIR_ADMIN . 'columns/tournaments.php';    
 include BORAHH_ADL_TOURNAMENTS_DIR_ADMIN . 'fields/tournaments.php';    
 

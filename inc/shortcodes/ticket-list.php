6ffd8f197a0ea570855bfd946878bcaada17f640
Himanshu Borah <borahhimanshu3@gmail.com>
2022-08-02 22:26:23 +0530

    ======

--- inc/shortcodes/ticket-list.php
+++ inc/shortcodes/ticket-list.php
@@ -1,19 +1,19 @@
 <?php
 
-add_shortcode( 'ticket-list', function( $a ) {
-    $a = shortcode_atts( array(
+add_shortcode( 'ticket-list', function( $atts ) {
+    $atts = shortcode_atts( array(
         'ID' => 0,
-    ), $a, 'ticket-list' );
+    ), $atts, 'ticket-list' );
  
     
-        echo $a['ID'];
+        echo $atts['ID'];
         $the_query = new WP_Query( array(
             'post_type' => 'product',
             'tax_query' => array(
                 array (
                     'taxonomy' => 'product_cat',
                     'field' => 'id',
-                    'terms' => $a['ID'],
+                    'terms' => $atts['ID'],
                 )
             ),
         ));

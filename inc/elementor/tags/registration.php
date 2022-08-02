5a20b60aeeab5ea5b2e9f8d6a28669da3d49e7f2
Himanshu Borah <borahhimanshu3@gmail.com>
2022-08-02 21:26:49 +0530

    ---

---
+++ inc/elementor/tags/registration.php
@@ -0,0 +1,74 @@
+<?php
+
+class Elementor_Dynamic_Entry extends \Elementor\Core\DynamicTags\Data_Tag {
+
+	/**
+	 * Get dynamic tag name.
+	 *
+	 * Retrieve the name of the random number tag.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Dynamic tag name.
+	 */
+	public function get_name() {
+		return 'entry';
+	}
+
+	/**
+	 * Get dynamic tag title.
+	 *
+	 * Returns the title of the random number tag.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return string Dynamic tag title.
+	 */
+	public function get_title() {
+		return esc_html__( 'Entry' );
+	}
+
+	/**
+	 * Get dynamic tag groups.
+	 *
+	 * Retrieve the list of groups the random number tag belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return array Dynamic tag groups.
+	 */
+	public function get_group() {
+		return [ 'tournaments' ];
+	}
+
+	/**
+	 * Get dynamic tag categories.
+	 *
+	 * Retrieve the list of categories the random number tag belongs to.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return array Dynamic tag categories.
+	 */
+	public function get_categories() {
+		return [ 
+            \Elementor\Modules\DynamicTags\Module::URL_CATEGORY,
+            \Elementor\Modules\DynamicTags\Module::POST_META_CATEGORY,
+        ];
+	}
+
+	/**
+	 * Render tag output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @return void
+	 */
+	public function get_value( array $options = array() ) {
+        $value = rtrim(get_permalink(),'/') . '-registration/';
+        return $value;
+	}
+
+}

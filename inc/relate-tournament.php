f17e8325ab5077909a8ee91ccf5010d37293b940
Himanshu Borah <borahhimanshu3@gmail.com>
2022-08-02 09:45:03 +0530

    --- CHANGES ---

--- inc/relate-tournament.php
+++ inc/relate-tournament.php
@@ -30,15 +30,15 @@ add_action('transition_post_status', function( $new_status, $old_status, $post )
         
         $sc = '[wpforms id="' . $form . '"]';
 
-        $entryID = wp_insert_post( array(
-            'post_type' => 'entry-page',
-            'post_title' => get_the_title($post->ID) . ' Entry',
-            'post_content' => $sc,
-            'post_status' => 'publish'
-        ));
+        // $entryID = wp_insert_post( array(
+        //     'post_type' => 'entry-page',
+        //     'post_title' => get_the_title($post->ID) . ' Entry',
+        //     'post_content' => $sc,
+        //     'post_status' => 'publish'
+        // ));
 
+        // update_field('entry_id', $entryID, $post_id);
        update_field('woo_ticket_id', $term['term_id'], $post_id);
-       update_field('entry_id', $entryID, $post_id);
        update_field('form_id', $form, $post_id);
   
       }

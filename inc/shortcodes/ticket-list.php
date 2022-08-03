aded27ed1dc26426440bb4194fd2855feb9a2313
Himanshu Borah <borahhimanshu3@gmail.com>
2022-08-03 19:05:55 +0530

    ===

--- inc/shortcodes/ticket-list.php
+++ inc/shortcodes/ticket-list.php
@@ -1,31 +1,41 @@
 <?php
 
-add_shortcode( 'ticket-list', function( $atts = '' ) {
-    $atts = shortcode_atts( array(
-        'ID' => 0,
-    ), $atts );
+function wpdocs_bartag_func( $atts ) {
+    $atts = shortcode_atts(
+        array(
+            'ID' => 'no foo',
+        ), $atts, 'bartag' );
+ 
+    return 'bartag: ' . esc_html( $atts['foo'] );
+}
+add_shortcode( 'ticket-list', 'wpdocs_bartag_func' );
+
+// add_shortcode( 'ticket-list', function( $atts = '' ) {
+//     $atts = shortcode_atts( array(
+//         'ID' => 0,
+//     ), $atts );
  
     
-        echo $atts['ID'];
-        $the_query = new WP_Query( array(
-            'post_type' => 'product',
-            'tax_query' => array(
-                array (
-                    'taxonomy' => 'product_cat',
-                    'field' => 'id',
-                    'terms' => $atts['ID'],
-                )
-            ),
-        ));
+//         echo $atts['ID'];
+//         $the_query = new WP_Query( array(
+//             'post_type' => 'product',
+//             'tax_query' => array(
+//                 array (
+//                     'taxonomy' => 'product_cat',
+//                     'field' => 'id',
+//                     'terms' => $atts['ID'],
+//                 )
+//             ),
+//         ));
 
-        if ( $the_query->have_posts() ) {
-            while ( $the_query->have_posts() ) {
-                $the_query->the_post();
-                echo the_title();
+//         if ( $the_query->have_posts() ) {
+//             while ( $the_query->have_posts() ) {
+//                 $the_query->the_post();
+//                 echo the_title();
 
-            }
+//             }
 
-        } else {
-            return "No Tickets available";
-        }
-} );
+//         } else {
+//             return "No Tickets available";
+//         }
+// } );

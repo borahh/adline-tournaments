131743bc434aee88d2474739843fbad7807b7f1a
Himanshu Borah <borahhimanshu3@gmail.com>
2022-08-03 21:11:06 +0530

    ===

--- admin/menu/menu.php
+++ admin/menu/menu.php
@@ -16,19 +16,19 @@ add_action( 'admin_menu', function() {
         6
     );
 
-	acf_add_options_sub_page(array(
+	add_submenu_page(
+        'manage_tournaments',
+        'Create Tournament',
+        'Create Tournament',
+        'manage_options',
+        'post-new.php?post_type=tournament',
+    );
+
+    acf_add_options_sub_page(array(
 		'page_title' 	=> 'Settings',
 		'menu_title'	=> 'Tournament Settings',
 		'parent_slug'	=> 'manage_tournaments',
 	));
-
-	add_submenu_page(
-        'manage_tournaments',
-        'Settings',
-        'Settings',
-        'manage_options',
-        'post-new.php?post_type=tournament',
-    );
 
 
 	/**

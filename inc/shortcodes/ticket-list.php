ea11375d9f938696bff293bd01d0bd00f978ce4e
Himanshu Borah <borahhimanshu3@gmail.com>
2022-08-03 19:13:14 +0530

    ===

--- inc/shortcodes/ticket-list.php
+++ inc/shortcodes/ticket-list.php
@@ -5,36 +5,28 @@ add_shortcode( 'ticket-list', function( $atts ) {
         array(
             'q' => 'no foo',
         ), $atts, 'ticket-list' );
+
+                $the_query = new WP_Query( array(
+                    'post_type' => 'product',
+                    'tax_query' => array(
+                        array (
+                            'taxonomy' => 'product_cat',
+                            'field' => 'id',
+                            'terms' => $atts['q'],
+                        )
+                    ),
+                ));
+
+                if ( $the_query->have_posts() ) {
+                    while ( $the_query->have_posts() ) {
+                        $the_query->the_post();
+                        echo the_title();
+
+                    }
+
+                } else {
+                    return "No Tickets available";
+                }
  
-    return 'ticket-list: ' . esc_html( $atts['q'] );
-} );
+});
 
-// add_shortcode( 'ticket-list', function( $atts = '' ) {
-//     $atts = shortcode_atts( array(
-//         'ID' => 0,
-//     ), $atts );
- 
-    
-//         echo $atts['ID'];
-//         $the_query = new WP_Query( array(
-//             'post_type' => 'product',
-//             'tax_query' => array(
-//                 array (
-//                     'taxonomy' => 'product_cat',
-//                     'field' => 'id',
-//                     'terms' => $atts['ID'],
-//                 )
-//             ),
-//         ));
-
-//         if ( $the_query->have_posts() ) {
-//             while ( $the_query->have_posts() ) {
-//                 $the_query->the_post();
-//                 echo the_title();
-
-//             }
-
-//         } else {
-//             return "No Tickets available";
-//         }
-// } );
